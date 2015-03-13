7a21fa7df89ec826b6967eafa80919f047a1ba74
Geoff Winner <Geoff@Geoffs-MacBook-Pro.local>
2015-03-13 12:08:16 -0700

    take user input and count the instances of the chosen word in a sentence

--- app/app.php
+++ app/app.php
@@ -9,15 +9,15 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__.'/../views'
 
     ));
-
+    //renders the home page with forms
     $app->get('/', function() use ($app) {
         return $app['twig']->render('form.twig');
     });
-
+    //renders the results page with home button
     $app->post('/results', function() use ($app) {
-      $new_count = new WordCount;
-      $count = $new_count->countWords($_POST['count']);
-      return $app['twig']->render('results.twig');
+      $new_wordCount = new WordCount;
+      $count = $new_wordCount->countWords($_POST['input1'], $_POST['input2']);
+      return $app['twig']->render('results.twig', array('count' => $count));
 
     });
 
